0794aa65ab63cdd44dfde8b07e7bef3e5e4c14c3
Mario Chois <mario.chois.navarro@gmail.com>
2021-08-10 22:58:17 -0500

    Cleared boiling pot files

---
+++ src/components/calculator.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+export default class Calculator extends React.Component {
+  render() {
+    return (
+      <div className="calc_container">
+        <div className="result"><p>0</p></div>
+        <div className="calc-buttons">
+          <div className="left-btns-container">
+            <button type="button" className="left-btn">AC</button>
+            <button type="button" className="left-btn">+/-</button>
+            <button type="button" className="left-btn">%</button>
+            <button type="button" className="left-btn">7</button>
+            <button type="button" className="left-btn">8</button>
+            <button type="button" className="left-btn">9</button>
+            <button type="button" className="left-btn">4</button>
+            <button type="button" className="left-btn">5</button>
+            <button type="button" className="left-btn">6</button>
+            <button type="button" className="left-btn">1</button>
+            <button type="button" className="left-btn">2</button>
+            <button type="button" className="left-btn">3</button>
+            <button type="button" className="left-btn-cero">0</button>
+            <button type="button" className="left-btn">.</button>
+          </div>
+          <div className="right-btns-container">
+            <button type="button" className="right-btn">+</button>
+            <button type="button" className="right-btn">x</button>
+            <button type="button" className="right-btn">-</button>
+            <button type="button" className="right-btn">+</button>
+            <button type="button" className="right-btn">=</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
