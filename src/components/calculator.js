56239214a722ae88857766c1d2048aa1cbd18a35
hunter4466 <mario.chois.navarro@gmail.com>
2021-08-11 11:34:57 -0500

    I have fixed all the suggestions from code review

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // eslint-disable-next-line react/prefer-stateless-function
-export default class Calculator extends React.Component {
+export default class Calculator extends Component {
   render() {
     return (
       <div className="calc_container">
